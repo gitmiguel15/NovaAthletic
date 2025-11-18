bc6d1584755c1dbc5e930b6bc8fa740e804ce7d9
Miguel Mantari <git.miguel15@gmail.com>
2025-11-18 18:34:52 -0500

    Update Carrito.java

--- src/Carrito.java
+++ src/Carrito.java
@@ -1,1 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrito {
+    private List<Producto> productos;
+
+    public Carrito() {
+        productos = new ArrayList<>();
+    }
+
+    public void agregarProducto(Producto producto) {
+        productos.add(producto);
+    }
+
+    public double calcularTotal() {
+        double total = 0.0;
+        for (Producto p : productos) {
+            total += p.getPrecio();
+        }
+        return total;
+    }
+}
 
