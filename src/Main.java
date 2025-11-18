01295578d062ce591e7bb61e67b56e759e9a3cd3
Miguel Mantari <git.miguel15@gmail.com>
2025-11-18 18:35:29 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        Producto p1 = new Producto("Camiseta deportiva", 59.90);
+        Producto p2 = new Producto("Short de entrenamiento", 79.90);
+        Producto p3 = new Producto("Zapatillas running", 199.90);
+
+        Carrito carrito = new Carrito();
+        carrito.agregarProducto(p1);
+        carrito.agregarProducto(p2);
+        carrito.agregarProducto(p3);
+
+        double total = carrito.calcularTotal();
+
+        System.out.println("Total de la compra: S/ " + total);
     }
 }
