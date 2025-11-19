a6599fb1f81f8ed91627d28b4054346754ebdc96
Miguel Mantari <git.miguel15@gmail.com>
2025-11-18 19:14:06 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+
         Carrito carrito = new Carrito();
-        carrito.agregarProducto(new Producto("Camiseta", 50.0));
-        carrito.agregarProducto(new Producto("Pantalón", 80.0));
+
+        carrito.agregarProducto(new Producto("Camiseta deportiva", 59.90));
+        carrito.agregarProducto(new Producto("Short de entrenamiento", 79.90));
+        carrito.agregarProducto(new Producto("Zapatillas running", 199.90));
+
+        // Aplicar descuento del 10% (0.10)
+        carrito.aplicarDescuento(0.10);
 
         double total = carrito.calcularTotal();
-        double totalConIGV = carrito.calcularTotalConIGV();
+        double igv = carrito.calcularIGV();
+        double descuento = carrito.calcularDescuento();
+        double totalFinal = carrito.calcularTotalFinal();
 
-        System.out.println("Total sin IGV: " + total);
-        System.out.println("Total con IGV: " + totalConIGV);
+        System.out.println("Total sin IGV: S/ " + total);
+        System.out.println("IGV (18%): S/ " + igv);
+        System.out.println("Descuento (10%): S/ " + descuento);
+        System.out.println("Total Final: S/ " + totalFinal);
     }
 }
